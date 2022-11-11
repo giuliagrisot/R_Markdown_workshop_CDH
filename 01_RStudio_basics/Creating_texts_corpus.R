87e906d330fa55bc04e17bb48cd8c9f986d3f250
Giulia Grisot <41911971+giuliagrisot@users.noreply.github.com>
2022-11-11 16:32:32 +0000

    news

---
+++ 01_RStudio_basics/Creating_texts_corpus.R
@@ -0,0 +1,219 @@
+# How to create a corpus of texts for analyses with R Studio -----
+
+# RECAP:
+
+# This is an R script file, created by Giulia (reads like English "Julia")
+
+# Everything written after an hashtag is a comment (normally appears in green). If you don't want to type the hash manually every time, you can type your comments normally, and after you finish, with the cursor on the sentence, press ctrl+shift+c. it will turn text into a comment and vice versa.
+
+# Everything else is R code. To execute the code, place the cursor on the corresponding line and press Ctrl+Enter (windows)
+
+# To complete this script, you will not need much knowledge of R: the codes are provided for you.
+# If you are unfamiliar with R language and basic operations and want to learn more about it, there is plenty of tutorials online. Have a look at the resources at the end of this script for a few recommendations.
+
+# before you start, check the working directory!
+# you can click on the Files panel, go to the Bielefeld_Hackaton_2022 folder, and once you are inside click on the little arrow near the "More" button, and select "Set as working directory"
+
+
+# now we're ready to start!
+
+
+
+
+# PS: Have you noticed that there is a little symbol on the top right of this panel, made of little horizontal lines? It shows and hide the document outline. if you write a comment and put a series of --------- (any number, more than 4) after it, the comment will become the header of a section, which you can then see in the outline for an easier navigation of the script.
+
+
+
+# Creating your dataset ----------
+
+# Often, one of the factors that prevents us humanists from working with RStudio for analysing texts is that tutorials assume a certain amount of (magically) pre-existing knowledge. 
+# Unfortunately, we often do not have that, nor a magic wand.
+# So it happens that right when you want to finally try to use someone else's existing scripts and make them work with your lovely literary texts (yes, thas's how we often do, and it's ok!), you are not really sure how to put those books into a shape that you can use.
+
+# Here we will try and show how different text formats can be imported in R and made ready for some analysis.
+
+## packages -----
+
+
+# Before you begin you will need to load some packages. These allow you to execute specific operations.
+# If you have not done so already, you have to install them first: it might take a few minutes and you only have to do it once. If R asks you whether you want to install dependencies for the packages, say yes.
+
+install.packages("tidyverse")
+install.packages("readr")
+install.packages("data.table")
+install.packages("tm")
+install.packages("tidytext")
+install.packages("syuzhet")
+install.packages("sjPlot")
+install.packages("wordcloud")
+install.packages("readxl")
+
+# Once you have installed the packeges you can comment the installation code like this:
+
+#   install.packages("blablabla")
+
+# so this operation will not be execute again in the future.
+
+
+library(tidyverse)
+library(readxl)
+library(readr)
+library(data.table)
+library(tm)
+library(tidytext)
+library(sjPlot)
+library(wordcloud)
+
+
+# Importing data ----
+
+## txt ----
+
+# One easy way to import texts into R is to start from txt files.
+
+# You might have more than one, so it is important that you store them all together in one folder, and ideally with a consistent filename. Information in the filename can be used later on to add metadata to your dataset. The format "surname_title_year.txt" could be a good option, for example, where the surname and the title have to be one word.
+
+# In order to import a txt file, you can use the "read.delim" function from base R (which means you do not need to install extra packages). 
+
+# let's try it out. As you can see in the files panels, there is a folder called "samples", where some texts in different formats are stored.
+
+# before you execute the code, make sure the working directory is set to "R_Markdown_workshop_CDH"
+
+
+pride <- read.delim("samples/austen_pride_1813.txt", # this is the url to your file
+                    fileEncoding = "utf-8",  # we want to read it as unicode text
+                    header = F) %>% # we do not want the first line as a header 
+  rename(text = V1) # we can name the column text
+
+# your file has been imported! 
+# Have a look at the first rows to see how it looks
+# execute the next code cunk or click on the "pride" element in your environment
+
+head(pride)
+
+
+
+
+# when importing a txt file, the paragraphs are automatically converted into new rows. if you want to have a single string instead, you can transform it, telling R to combine the rows, and to add "\n " (a conventional code for a new line) between one piece of string and the next, as follows:
+
+pride_whole <- paste(unlist(pride), collapse ="\n")
+
+head(as_tibble(pride_whole))
+
+pride_whole_tibble <- as_tibble(pride_whole)
+
+# you can then split it into sentences, for instance with packages syuzhet (the result will be a list of strings)
+
+pride_sentences <- get_sentences(pride_whole)
+
+head(as_tibble(pride_sentences))
+
+# or with the package tidytext (this will turn into a dataframe)
+
+pride_sentences <- unnest_sentences(pride, 
+                                    input = text,
+                                    output = "sentence_text",
+                                    to_lower = F)
+
+head(pride_sentences)
+
+## multiple txt files ----------
+
+# if you have more than one text, you probably won't want to repeat this operations manually several times.
+# you can then proceed as follows:
+# (this is just one way but there are many out there)
+
+# - create a silt of the files inside the folder that match the criteria (txt)
+
+corpus_files <- list.files(path = "samples", pattern = "*.txt", full.names = T)
+
+
+corpus_source <- corpus_files %>%  
+  set_names(.) %>% 
+  map_df(read.delim, fileEncoding = "utf-8", 
+         .id = "FileName", 
+         header = F) %>%
+  rename(text = V1)
+
+
+head(corpus_source)
+
+
+# let's see which files are in our corpus:
+
+corpus_source %>% 
+  select(FileName) %>%
+  distinct()
+
+
+# now, as we mentioned you might want to use the information in the filename to create more variables (that's how "columns" are called in R) in our corpus
+
+corpus <- corpus_source %>%
+  mutate(FileName = str_remove_all(FileName, "samples/")) %>% # let's remove the directory from the filename
+  separate(FileName, into = c("author", "title", "year"), sep = "_", remove = T) %>% # and separate the metadata
+  mutate(year = str_remove(str_trim(year, side = "both"), ".txt")) # and make sure there are no extra spaces before/after the words
+
+corpus$year <- as.numeric(corpus$year)
+
+# click on corpus and see how it looks. Neat, right?
+
+# you might also want to add an identification number for the sentences, which can be useful for later analysis
+
+corpus <- corpus %>%
+  group_by(title) %>%
+  mutate(sentence_id = seq_along(text)) %>%
+  ungroup()
+
+
+head(corpus, 5)
+
+
+## csv and xslx ----
+
+# another common format for texts is csv or xlsx. Importing a this is very easy, because R understands the csv and xslx formats well. You can either use code, or click directly on the file you want to import in the files panel.
+# R studio will ask if you want to import it, and you will be able to determine options with a friendly interface.
+
+# navigate into the samples folder and click on the file small_corpus.xlsx. or 
+# execute the following code
+
+pride_excel <- read_excel("samples/pride.xlsx")
+
+# have a look at it
+
+head(pride_excel)
+
+
+## multiple files ----
+
+# the procedure similar to the one we saw for the txt files, except it has read_excel as function, and it does not need to add a header or other variables
+
+corpus_files <- list.files(path = "samples", pattern = "*.xlsx",  full.names = T)
+
+
+corpus_source <- corpus_files %>%  
+  set_names(.) %>% 
+  map_df(read_excel)
+
+head(corpus_source)
+
+## epubs ----------
+
+## another format that is quite popular today, especially for books, is the epub
+## for these files you will need a specific package, calle "epubr"
+
+install.packages("epubr")
+
+library(epubr)
+
+kafka_all <- epubr::epub("samples/kafka.epub")
+
+# have a look a the dataset "kafka_all": epubs often have a more complex internal structure, and you might need to modify your dataset according to your needs
+
+kafka_werke <- kafka_all[[9]][[1]]
+
+# the content is often nested and you really need to know what you are looking for to extract it
+
+kafka_werke %>%
+  filter(nword!=0) %>%
+  head()
+
--- 01_RStudio_basics/R_intro_script.R
+++ 01_RStudio_basics/R_intro_script.R
@@ -1,20 +1,31 @@
 ### Welcome!
 
-# This is an R script file, created by Simone
-# Everything written after an hashtag is a comment
+# This is an R script file
+# Everything written after an hash symbol (#) is a comment
 # Everything else is R code
-# To activate the code, place the cursor on the corresponding line
+
+# To execute the code, place the cursor on the corresponding line
 # (or highlight multiple lines/pieces of code) 
 # ...and press Ctrl+Enter (or Cmd+Enter for Mac)
-# (the command will be automatically copy/pasted into the console)
+
+# (the command will be automatically "copy/pasted" into the console and executed)
 
 ### 1. Creating variables
 
+# with R, you can create various kinds of "objects", that can then be manipualted and stored.
+# these objects must be named, and can be named however you fancy: be careful though, because object names CANNOT contain spaces.
+# The standard naming practices use short but meaningful labesl, in the formats word_word (ex. giulia_object), word_number (giulia_1), or wordWord (giuliaObject), wordN (giulia1).
+# R is CASE SENSITIVE: an object called "giulia_object" will not considered the same as an object called Giulia_object.
+
+
+
+# several types of objects:
+
 # numbers
 my_number <- 1
-my_number
+my_number # nb. executing an object alone will "print" it in the console
 
-# strings of text
+# strings of text (note that strings must be enclosed in quotation marks)
 my_string <- "to be or not to be"
 my_string
 
@@ -55,6 +66,33 @@ my_new_df <- data.frame(
   author = c(XXXXXX), 
   nationality = c(XXXXX))
 View(my_new_df)
+
+
+## Working directory
+
+# It is very important, as a first step of any project, to check that
+# you are working in the right directory.
+# try and run this code:
+
+getwd() # getwd is the name of a function, and it tells you it just means "get working directory"
+
+# you will see the working directory you are currently in in the console
+# if you want to change the wd, you can do so with the "setwd" function.
+# for example:
+
+setwd("corpus") # this will make the subfolder "corpus" (already in your project) your working directory
+# have a look to check
+getwd()
+
+
+# EXERCISE: Now try and reset the working directory to the original folder (the previous one)
+setwd(XXXX)
+
+# you can achieve the same result from the "Files" quadrant of RStudio:
+# just navigate inside the folder you'd like to set as wd, click on the little 
+# blue wheel on the top right, and click on "Set as working directory".
+# You will see that the same formula will appear in the console.
+
 
 
 ### 2. Accessing variables
@@ -115,33 +153,19 @@ substr()
 
 ### 4. Reading/writing text files
 
-# printing working directory
+#check where your wd is
 getwd()
-setwd("/cloud/project/scripts") # notice that this can also be done via the RStudio GUI
-getwd()
-setwd("/cloud/project")
+# and set "corpus" as wd
+setwd()
 
 # read text line by line
-my_text <- readLines("scripts/samples/federer_pilatus_1912.txt")
-head(my_text)
+my_text <- readLines("Cbronte_Jane_1847.txt")
+head(my_text, 5)
 
-# collapse all text in a single line (separated by the "newline" character)
-my_text <- paste(my_text, collapse = "\n")
 
-# print text / write file
-print("The cat is on the table")
+# write file
 cat("The cat is on the table")
-cat("The cat is on the table", my_string)
-cat("The cat is on the table", file = "scripts/samples/Cat.txt")
-
-### Your Turn (4) - start
-
-# read another .txt file in the "samples" folder
-# and split it into single words 
-my_text <- readLines("")
-strsplit()
-
-### Your Turn (4) - end
+cat("The cat is on the table", file = "Cat.txt")
 
 ### 5. Making loops
 
@@ -166,33 +190,37 @@ for(i in 1:10){
   }
 }
 
-### Your Turn (5) - start
+# the sapply/lapply functions
+# (with a simple example: increase the values in one vector)
+my_vector <- 1:10
+my_vector
+for(i in 1:10){
+  
+  my_vector[i] <- my_vector[i]+1
+  
+}
+my_vector
 
-# write a loop that prints numbers until 4, then prints "more than four"
-# tip: copy paste here below the "more than one" loop and modify it
+# it is the same of this sapply function:
+my_vector <- 1:10
+my_vector <- sapply(my_vector, function(x) x+1)
+my_vector
 
+# if you are working with lists, then you can use lapply
+my_list <- list(1:10, 2:20)
+my_list <- lapply(my_list, function(x) x+1)
+my_list
 
-### Your Turn (5) - end
+# why use sapply/lapply? Because they are faster than a loop
 
 ### 6. Functions
 
 # basic (stupid) example
-my_function <- function(x){
-  cat("Ciao", x)
+my_function <- function(){
+  cat("Ciao")
 }
 
-my_function("Simone")
-my_function("Giulia")
-
-### Your Turn (6) - start
-
-# create a function that takes as input two names 
-# (e.g. "Simone" and "Giulia")
-# ...and prints a sentence including those two names
-# (e.g. "Simone is teaching R with Giulia")
-
-
-### Your Turn (6) - end
+my_function()
 
 ### 7. Packages
 
@@ -210,3 +238,13 @@ my_df[which(my_df$nationality == "Italian"),]
 # with tidyverse, you do like
 my_df %>% filter(nationality == "Italian")
 
+### Appendix
+# note: the "<-" sign can be substitited by "="
+my_variable <- "Shakespeare"
+my_variable = "Shakespeare"
+# still, it is advised to distinguish between the two, as the "<-" sign has a "stronger" function. See for example in the creation of a dataframe
+my_df <- data.frame(author = c("Shakespeare", "Dante", "Cervantes", "Pynchon"), nationality = c("English", "Italian", "Spanish", "American"))
+author # it does not exist!!
+
+my_df_2 <- data.frame(author <- c("Shakespeare", "Dante", "Cervantes", "Pynchon"), nationality <- c("English", "Italian", "Spanish", "American"))
+author # now it exists!!
